6084a5ebfe73f399bb7d6b65fbc68d7c9cd4249d
naamiru <152694159+naamiru@users.noreply.github.com>
2023-12-10 09:36:55 +0900

    memoize killer handler

--- client/src/options/KillerOption.tsx
+++ client/src/options/KillerOption.tsx
@@ -1,18 +1,20 @@
-import { type ReactNode } from 'react'
+import { useCallback, useMemo, type ChangeEvent, type ReactNode } from 'react'
 import { useApp } from '../app/use-app'
 
 export default function KillerOptions(): ReactNode {
   const { killers, dispatch } = useApp()
 
-  function handleChange(i: 0 | 1 | 2, value: number): void {
-    const values = [...killers] as [number, number, number]
-    values.splice(i, 1, value)
-    dispatch({ type: 'setKillers', value: values })
-  }
+  const changeHandlers = useMemo(() => {
+    return [0, 1, 2].map(i => (event: ChangeEvent<HTMLSelectElement>) => {
+      const values = [...killers] as [number, number, number]
+      values.splice(i, 1, parseInt(event.target.value, 10))
+      dispatch({ type: 'setKillers', value: values })
+    })
+  }, [dispatch, killers])
 
-  function reset(): void {
+  const reset = useCallback(() => {
     dispatch({ type: 'setKillers', value: [0, 0, 0] })
-  }
+  }, [dispatch])
 
   return (
     <div className="option killer-option">
@@ -29,12 +31,7 @@ export default function KillerOptions(): ReactNode {
           </p>
           <p className="control">
             <span className="select is-small">
-              <select
-                value={killers[0]}
-                onChange={e => {
-                  handleChange(0, parseInt(e.target.value, 10))
-                }}
-              >
+              <select value={killers[0]} onChange={changeHandlers[0]}>
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -49,12 +46,7 @@ export default function KillerOptions(): ReactNode {
           </p>
           <p className="control">
             <span className="select is-small">
-              <select
-                value={killers[1]}
-                onChange={e => {
-                  handleChange(1, parseInt(e.target.value, 10))
-                }}
-              >
+              <select value={killers[1]} onChange={changeHandlers[1]}>
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -69,12 +61,7 @@ export default function KillerOptions(): ReactNode {
           </p>
           <p className="control">
             <span className="select is-small">
-              <select
-                value={killers[2]}
-                onChange={e => {
-                  handleChange(2, parseInt(e.target.value, 10))
-                }}
-              >
+              <select value={killers[2]} onChange={changeHandlers[2]}>
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
